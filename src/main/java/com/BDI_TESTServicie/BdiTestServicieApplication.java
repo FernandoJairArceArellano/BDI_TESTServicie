5c384797c4541a46f984628e61cb94292c169352
Alien 9 <Alien 9@ALIEN9>
2025-07-01 19:01:32 -0600

    Primer commit

---
+++ src/main/java/com/BDI_TESTServicie/BdiTestServicieApplication.java
@@ -0,0 +1,13 @@
+package com.BDI_TESTServicie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdiTestServicieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdiTestServicieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BDI_TESTServicie/BdiTestServicieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BDI_TESTServicie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdiTestServicieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
